9ea411199a67df7add76a4b10d18ed064af3ec3d
kibria30 <bsse1430@iit.du.ac.bd>
2023-08-17 01:31:09 +0600

    Human vs human (console) (unless gameover()) done

--- Checkers_Game.cpp
+++ Checkers_Game.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 char checkerBoard[BOARD_SIZE][BOARD_SIZE];
-bool redTurn;     // identify whose move
+bool redTurn = true;     // identify whose move
 
 struct Move{
     int fromRow, fromCol, toRow, toCol;
@@ -18,13 +18,63 @@ struct Move{
 void initiateBoard();
 void printBoard();
 Move getMove();
-bool isValidMove(Move move);
+bool isValidMove(Move move);  
+void play();     
+bool isGameOver();  // not start
+void makeMove(Move move); 
+void promote(int row, int col); 
+
 
 int main(){
-    
+
     initiateBoard();
-    printBoard();
+    play();
      
+}
+
+void play(){
+
+    while(!isGameOver()){
+        printBoard();
+        cout<< (redTurn ? "Red's " : "Blue's ") <<"turn:"<<endl;
+        Move move;
+        do{
+            move = getMove();
+        }while(!isValidMove(move));
+        makeMove(move);
+    }
+    printBoard();
+    cout<<"Gameover!!"<<endl;
+}
+
+void makeMove(Move move){
+    int fromRow = move.fromRow, fromCol = move.fromCol, toRow = move.toRow, toCol = move.toCol;
+
+    char piece = checkerBoard[fromRow][fromCol];
+    checkerBoard[fromRow][fromCol] = EMPTY;
+    checkerBoard[toRow][toCol] = piece;
+
+    if( abs(toRow - fromRow) == 2){
+        int jumpedRow = (fromRow + toRow)/2;
+        int jumpedCol = (fromCol + toCol)/2;
+        checkerBoard[jumpedRow][jumpedCol] = EMPTY;
+    }
+
+    if((redTurn && toRow == 0) || (!redTurn && toRow == BOARD_SIZE-1))
+        promote(toRow, toCol);
+
+    redTurn = !redTurn;
+}
+
+void promote(int row, int col){
+    if(checkerBoard[row][col] == BLUE_PIECE)
+        checkerBoard[row][col] = BLUE_KING;
+    else if(checkerBoard[row][col] == RED_PIECE)
+        checkerBoard[row][col] = RED_KING;
+}
+
+bool isGameOver(){ 
+    return false;
 }
 
 bool isValidMove(Move move){
@@ -40,17 +90,15 @@ bool isValidMove(Move move){
     if(checkerBoard[toRow][toCol] != EMPTY)
         return false;
     
-    int rowDiff = abs(toRow - fromRow), colDiff = abs(toCol - fromCol);
+    int rowDiff = abs(toRow - fromRow), colDiff = abs(toCol - fromCol); 
     if(rowDiff == 1 && colDiff == 1)
         return true;
-        
     else if(rowDiff == 2 && colDiff == 2){
-        int jumpedRow = (toRow + fromRow)/2, jumpedCol = (toCol - fromCol)/2;
+        int jumpedRow = (toRow + fromRow)/2, jumpedCol = (toCol + fromCol)/2;
         if((redTurn && (checkerBoard[jumpedRow][jumpedCol] == BLUE_PIECE || checkerBoard[jumpedRow][jumpedCol] == BLUE_KING))
             || (!redTurn && (checkerBoard[jumpedRow][jumpedCol] == RED_PIECE || checkerBoard[jumpedRow][jumpedCol] == RED_KING)))
-            return true;
+            return true;   
     }
-    cout<<"hello"<<endl;
     return false;
 }
 
@@ -75,7 +123,9 @@ void initiateBoard(){
 }
 
 void printBoard(){
+    cout << "  0 1 2 3 4 5 6 7" << endl;
     for(int row=0; row<BOARD_SIZE; row++){
+        cout<< row <<" ";
         for(int col=0; col<BOARD_SIZE; col++){
             if(!checkerBoard[row][col])
                 cout<<". ";
